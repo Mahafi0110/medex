e3f22fb3e23f667152097dac78e9be576dfea199
Mahafi0110 <mahafi1999@gmail.com>
2026-09-22 18:47:59 +0530

    Fix python version for django compatibility

--- backend/medex_backend/settings.py
+++ backend/medex_backend/settings.py
@@ -125,7 +125,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
